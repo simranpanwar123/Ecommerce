6069af3db18ce4a2e78fb08483b99a6b56f0d06f
simranpanwar01 <simmupanwar537@gmail.com>
2026-07-24 14:29:50 +0530

    update repo

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://ecommerce-backend-hut8.onrender.com/api"
-}); 
+  baseURL: "https://ecommerce-c19q.onrender.com/api",
+})
 
 export default api;
